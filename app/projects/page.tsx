c733d92ca39962dfa75bf433b9737ab18ef90f5f
ShinobiWPS <axanderghemov@gmail.com>
2025-11-01 19:48:34 +0100

    fix projects missing image

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,17 +7,15 @@ export const metadata = genPageMetadata({ title: 'Projects' })
 export default function Projects() {
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
+      <div className="dark:divide-gray-700 divide-y divide-gray-200">
+        <div className="md:space-y-5 pt-6 pb-8 space-y-2">
+          <h1 className="sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100 text-3xl font-extrabold leading-9 tracking-tight text-gray-900">
             Projects
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
-          </p>
+          <p className="dark:text-gray-400 text-lg leading-7 text-gray-500"></p>
         </div>
         <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
+          <div className="flex flex-wrap -m-4">
             {projectsData.map((d) => (
               <Card
                 key={d.title}
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,22 +9,8 @@ const projectsData: Project[] = [
   {
     title: 'Mempeak - Vite plugin to show Heap-memory usage',
     description: `A convienent way to see the real-time usage of heap-memory in JS`,
-    imgSrc: '/static/images/',
+    imgSrc: '/static/images/Vite.svg',
     href: 'https://www.npmjs.com/package/vite-plugin-mempeak',
-  },
-  {
-    title: 'Open Source contributions',
-    description: `Where I improved something publickly available`,
-    imgSrc: '/static/images/opensource_logo.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
