6119d6f1631e3fb96a1099d2516735cd71d26eed
ShinobiWPS <axanderghemov@gmail.com>
2025-11-01 22:28:26 +0100

    fix: prettier complains

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,15 +7,15 @@ export const metadata = genPageMetadata({ title: 'Projects' })
 export default function Projects() {
   return (
     <>
-      <div className="dark:divide-gray-700 divide-y divide-gray-200">
-        <div className="md:space-y-5 pt-6 pb-8 space-y-2">
-          <h1 className="sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100 text-3xl font-extrabold leading-9 tracking-tight text-gray-900">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             Projects
           </h1>
-          <p className="dark:text-gray-400 text-lg leading-7 text-gray-500"></p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400"></p>
         </div>
         <div className="container py-12">
-          <div className="flex flex-wrap -m-4">
+          <div className="-m-4 flex flex-wrap">
             {projectsData.map((d) => (
               <Card
                 key={d.title}
